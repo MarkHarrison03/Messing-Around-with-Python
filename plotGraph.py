8d8ef5dbbc0bbd94cc3c8a3099ddcbb17410ce65
MarkHarrison03 <21335265@studentmail.ul.ie>
2023-04-03 17:26:31 +0100

    made a graph program in python that displays my qca over time LOL

---
+++ plotGraph.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [1,2,3,]
+y = [3.64,3.7,3.92]
+plt.xlabel("Semester")
+plt.ylabel("QCA")
+plt.title("Growth expected to decline after semester 3")
+plt.plot(x, y)
+plt.show()
